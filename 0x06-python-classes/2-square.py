622c9097bb44a0e8ec234a5fd80a8d02e87605a5
GideonMissoy <giddykips2030@gmail.com>
2023-03-30 02:39:39 +0000

    Hot fix.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""A Square Class with type safty"""
+""" defines a square """
 
 
 class Square:
-    """initilizing the private attribute size by checking it's incomming type using exceptions"""
+    """ square with private instance attribute size """
+
     def __init__(self, size=0):
+        """
+        Args:
+            size: size of square
+        """
 
-        if type(size) is not int:
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
             raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
