1584d7a6c4b304dbed63f2cf42acde9f0b636307
GideonMissoy <missoykipsang@gmail.com>
2023-04-30 15:49:55 -0700

    Fixing if is float error.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,9 +7,9 @@ The 0-add_integer module supplies one function, add_integer(a, b=98).
 def add_integer(a, b=98):
     """Function to add two integer or float number and return their sum"""
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or a float")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or a float")
+        raise TypeError("b must be an integer")
 
     if isinstance(a, float):
         a = int(a) 
