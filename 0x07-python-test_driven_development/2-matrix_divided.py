7340ca1169b3c8f0d5d58cb882cf4979686d4625
GideonMissoy <missoykipsang@gmail.com>
2023-04-30 16:24:27 -0700

    Fixing pycode errors.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,11 +6,15 @@ def matrix_divided(matrix, div):
     """
     Divides a matrix by a scalar integer and rounds to two decimal places.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+            for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)
+                 of integers/floats")
 
-    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(element, (int, float))
+            for row in matrix for element in row):
+        raise TypeError("matrix must be a matrix (list of lists)
+                of integers/floats")
 
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -21,5 +25,6 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    new_matrix = [[round(element / div, 2) for element in row]
+            for row in matrix]
     return new_matrix
