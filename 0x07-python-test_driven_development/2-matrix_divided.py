36aaff689920aab2afd888a0a7391b743dab414d
GideonMissoy <missoykipsang@gmail.com>
2023-04-30 16:26:36 -0700

    Divide a matrix.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,15 +6,11 @@ def matrix_divided(matrix, div):
     """
     Divides a matrix by a scalar integer and rounds to two decimal places.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list)
-            for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists)
-                 of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    if not all(isinstance(element, (int, float))
-            for row in matrix for element in row):
-        raise TypeError("matrix must be a matrix (list of lists)
-                of integers/floats")
+    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -25,6 +21,5 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(element / div, 2) for element in row]
-            for row in matrix]
+    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
     return new_matrix
