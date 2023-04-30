235e218673d55b062caa8813aa59c94c68c023df
GideonMissoy <missoykipsang@gmail.com>
2023-04-30 16:06:47 -0700

    Updating the file, fixing possible float errors.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,6 +4,7 @@ This is the "0-add_integer" module.
 The 0-add_integer module supplies one function, add_integer(a, b=98).
 """
 
+
 def add_integer(a, b=98):
     """Function to add two integer or float number and return their sum"""
     if not isinstance(a, (int, float)):
@@ -12,8 +13,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     if isinstance(a, float):
-        a = int(a) 
+        a = int(a)
     if isinstance(b, float):
         b = int(b)
 
-    return a + b
+    return (a + b)
