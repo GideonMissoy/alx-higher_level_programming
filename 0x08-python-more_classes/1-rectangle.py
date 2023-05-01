efd6c85ea084d7f963a8f7915f2779cc77afa927
GideonMissoy <missoykipsang@gmail.com>
2023-05-01 07:15:06 -0700

    Fixing ValueError.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,38 +6,38 @@ class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
 
 class Rectangle:
     """A class that defines a rectangle by its width and height"""
-    
+
     def __init__(self, width=0, height=0):
         """Initialize the rectangle with the given width and height"""
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Get the width of the rectangle"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set the width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("Width must be an integer")
         elif value < 0:
-            raise ValueError("Width must be non-negative")
+            raise ValueError("Width must be >= 0")
         else:
             self.__width = value
-    
+
     @property
     def height(self):
         """Get the height of the rectangle"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("Height must be an integer")
         elif value < 0:
-            raise ValueError("Height must be non-negative")
+            raise ValueError("Height must be >= 0")
         else:
             self.__height = value
