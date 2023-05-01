b68ad8c1d17a9b91c02d516f63c90007e7531882
GideonMissoy <missoykipsang@gmail.com>
2023-04-30 17:52:04 -0700

    Program iteration

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
 
-def text_indentation(text):
-    """
-    Prints the input text with two newlines after each of these characters:
-    '.', '?' and ':'.
-    """
+def print_text(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    # Replace sentence-ending punctuation with the punctuation,
-    # followed by two newlines
-    sentences = ''
+    
+    # Initialize a flag to indicate whether the previous character
+    #was a sentence-ending punctuation
+    prev_punct = False
+    
+    # Iterate over each character in the input string
     for char in text:
-        if char in '.?:':
-            sentences += char + '\n\n'
+        # If the character is a sentence-ending punctuation,
+        # print it with two newlines and set the flag to True
+        if char in ".?!":
+            print(char, end="")
+            print("\n\n", end="")
+            prev_punct = True
+        # If the character is a colon, print it with a newline and set the flag to True
+        elif char == ":":
+            print(char, end="")
+            print("\n\n", end="")
+            prev_punct = True
+        # If the character is not a sentence-ending punctuation or a colon, print it normally
         else:
-            sentences += char
-
-    # Split the resulting string into a list of lines,
-    # remove leading and trailing whitespace, and print each line
-    lines = [line.strip() for line in sentences.splitlines()]
-    for line in lines:
-        print(line)
+            # If the previous character was a sentence-ending punctuation,
+            # print the current character on a new line
+            if prev_punct:
+                print(char, end="")
+                print("\n", end="")
+                prev_punct = False
+            # If the previous character was not a sentence-ending punctuation,
+            # print the current character normally
+            else:
+                print(char, end="")
+                prev_punct = False
+    # If the last character was a sentence-ending punctuation or a colon, print an extra newline
+    if prev_punct:
+        print("\n", end="")
