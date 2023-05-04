1fac4014d9ac3d3426d891483609ebcd01583edd
GideonMissoy <missoykipsang@gmail.com>
2023-05-03 19:52:28 -0700

    13. Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Function adds a new attribute to an object
+Function defines a function `add_new_attr` that adds a new attribute to an object.
 """
 
 
 def add_new_attr(obj, attr, value):
-    """Attribute to add"""
+    """Adds a new attribute to an object.
+    TypeError: If the object does not have a `__setattr__` method."""
     if hasattr(obj, '__setattr__'):
     setattr(obj, attr, value)
     else:
