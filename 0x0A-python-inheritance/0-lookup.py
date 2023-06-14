5117200b05a68a96cae68499e171ee7fda29de6c
GideonMissoy <giddykips2030@gmail.com>
2023-06-14 21:31:49 +0000

    Fixing indentation.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,6 +6,6 @@ and methods of an object.
 
 
 def lookup(obj):
-"""Returns list of available attributes and methods."""
-return dir(obj)
+	"""Returns list of available attributes and methods."""
+	return dir(obj)
 
