5f68a444a6f56484f02d0930c5a0ef7d59eef6df
GideonMissoy <giddykips2030@gmail.com>
2023-06-14 21:41:26 +0000

    1. My list. A class MyList that inherits from list.

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """
-MyList inherits list
+MyList inherits list.
 """
 
 
 class MyClass(list):
-	"""Subclass of list initializer."""
+    """
+    Subclass of list that provides additional functionalities.
 
-	def __init__(self):
-		"""Initializes the object we have."""
+    This class extends the functionality of the built-in list class.
+    It adds a method to print the list in sorted order.
+    """
 
-		super().__init__()
-	
-	def print_sorted(self):
-		"""Prints the list, but sorted in ascending."""
-		print(sorted(self))
+    def __init__(self):
+        """
+        Initializes an instance of MyClass.
+        """
+        super().__init__()
+
+    def print_sorted(self):
+        """
+        This method sorts the list in ascending order using the sorted function
+        and then prints the sorted list.
+        """
+        print(sorted(self))
 
