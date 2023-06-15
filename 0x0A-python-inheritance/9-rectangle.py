9f0e99223d29121554edaeb7eb5c4693a467abdb
GideonMissoy <giddykips2030@gmail.com>
2023-06-15 21:35:52 +0000

    9. Full rectangle.

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,20 +8,19 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-	"""A representation of a rectangle."""
-	def __init__(self, width, height):
-		"""Initializes the rectangle."""
-		self.integer_validator("width", width)
-		self.integer_validator("height", height)
-		self.__width = width
-		self.__height = height
+    """A representation of a rectangle."""
+    def __init__(self, width, height):
+        """Initializes the rectangle."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
 
-	def area(self):
-		"""Returns the area of a rectangle."""
-		return self.__width * self.__height
+    def area(self):
+        """Returns the area of a rectangle."""
+        return self.__width * self.__height
 
-	def __str__(self):
-		"""Return string representing a rectangle."""
-		return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-
+    def __str__(self):
+        """Return string representing a rectangle."""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
