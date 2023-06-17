0663ab8084b3e220fef516da7981aa393e4e3401
GideonMissoy <giddykips2030@gmail.com>
2023-06-17 02:02:35 +0000

    4. From JSON string to Object.

---
+++ 0x0B-python-input_output/4-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from_json_string = __import__('4-from_json_string').from_json_string
+
+s_my_list = "[1, 2, 3]"
+my_list = from_json_string(s_my_list)
+print(my_list)
+print(type(my_list))
+
+my_dict = from_json_string(s_my_dict)
+print(my_dict)
+print(type(my_dict))
+
+try:
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
