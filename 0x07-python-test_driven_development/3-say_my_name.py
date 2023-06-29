edd41fc431f94e208b74b6b2223c04b7b83ad533
GideonMissoy <giddykips2030@gmail.com>
2023-06-29 02:43:43 +0000

    2. Say my name. This funtion prints a formatted name, with an optional last name.

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,5 +10,5 @@ def say_my_name(first_name, last_name=""):
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
-        raise TypeError("last name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is", first_name, last_name)
