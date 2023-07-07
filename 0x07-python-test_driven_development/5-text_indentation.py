09876f41f384260a39da063c75b8aeb3fffbc6bf
GideonMissoy <giddykips2030@gmail.com>
2023-07-07 12:23:08 -0700

    4. Text indentation. This function prints a text with 2 new lines after some special characters.

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,18 +7,10 @@ Function prints a text with 2 new lines after: ('.', '?', ':')
 
 def text_indentation(text):
     """prints a text with 2 new lines after . ? :"""
-    if type(text) is not str:
+    special = ['.', '?', ':']
+    if type(text) != str:
         raise TypeError("text must be a string")
-    flag = 0
-    for t in text:
-        if flag == 0:
-            continue
-        else:
-            flag = 1
-        if flag == 1:
-            if t == '?' or t == '.' or t == ':':
-                print(t)
-                print()
-                flag = 0
-            else:
-                print(t, end="")
+    for s in text:
+        print(s, end='')
+        if s in special:
+            print('\n\n', end='')
