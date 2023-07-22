2f6a5a9a1046119b19bf15271a49acb599d56546
GideonMissoy <giddykips2030@gmail.com>
2023-07-21 17:28:18 -0700

    8. Script lists all the cities of California that can be found in the database.

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Lists all cities in ascending of california in the database.
+SELECT `id`, `name`
+FROM `cities`
+WHERE `state_id` IN
+(SELECT `id`
+	FROM `states`
+	WHERE `name` = 'California')
+ORDER BY `id`;
