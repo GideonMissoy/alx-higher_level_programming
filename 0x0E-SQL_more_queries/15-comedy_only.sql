629090ad1fcc6a011224512662c35608ee87e922
GideonMissoy <giddykips2030@gmail.com>
2023-07-22 14:04:13 -0700

    15. Script lists all Comedy shows in the database.

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists in DESC all comdey shows in the database
+SELECT t.`title`
+FROM `tv_shows` AS t
+INNER JOIN `tv_show_genres` AS s
+ON t.`id` = s.`show_id`
+INNER JOIN `tv_genres` AS g
+ON g.`id` = s.`genre_id`
+WHERE g.`name` = 'Comedy'
+ORDER BY t.`title`;
