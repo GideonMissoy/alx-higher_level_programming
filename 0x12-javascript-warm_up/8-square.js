70365392e6a98a3d52e0ea0bd1ce30727bef4ed8
GideonMissoy <giddykips2030@gmail.com>
2023-09-02 22:43:32 -0700

    Hot fix.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,7 @@
 const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
-}
-else {
+} else {
   for (let r = 0; r < size; r++) {
     let row = '';
     for (let c = 0; c < size; c++) row += 'X';
