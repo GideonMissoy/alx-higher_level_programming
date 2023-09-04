c5847692940e191c0956354958057789a1eeaaa6
GideonMissoy <giddykips2030@gmail.com>
2023-09-04 03:01:02 -0700

    Script takes in an arg and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,9 +4,9 @@ This script lists all states with
 a `name` starting with the letter `N`
 from the database `hbtn_0e_0_usa`.
 """
-
 import MySQLdb
 from sys import argv
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,9 +4,9 @@ Script takes in an argument and displays all
 values in the states where `name` matches the
 argument from the database `hbtn_0e_0_usa`.
 """
-
 import MySQLdb
 from sys import argv
+
 
 if __name__ == '__main__':
     user, password, database, state = argv[1], argv[2], argv[3], argv[4]
