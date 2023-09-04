88e7492940894e28170ee9f4c550b7fc10f5e6bb
GideonMissoy <giddykips2030@gmail.com>
2023-09-04 04:53:18 -0700

    Advanced tasks: 15. City relationship #advanced

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
--- Create the database hbtn_0e_100_usa
-CREATE DATABASE
-IF NOT EXISTS hbtn_0e_100_usa;
-USE hbtn_0e_100_usa;
+"""
+Creates the State "California" with the 
+City "San Francisco" from a database
+"""
+from sys import argv
+from relationship_state import State, Base
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-SELECT *
-FROM states;
-SELECT *
-FROM cities;
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
+
+    session.add(newState)
+    session.add(newCity)
+    session.commit()
