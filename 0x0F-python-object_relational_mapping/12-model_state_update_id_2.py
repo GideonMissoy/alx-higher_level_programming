33769835d4604ea0d28fa8cba362c28fe1eed887
GideonMissoy <giddykips2030@gmail.com>
2023-09-04 04:13:07 -0700

    Some fixes then making the files executable.

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     instance = session.query(State).filter_by(id=2).first()
-    instance.name='New Mexico'
+    instance.name = 'New Mexico'
     session.commit()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,6 +11,7 @@ from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     citate = session.query(City, State).join(State)
     for city, state in citate.all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
