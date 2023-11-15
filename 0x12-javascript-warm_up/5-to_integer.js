afa4bcc00a865eea8953530b212c166c1755c2d9
GideonMissoy <giddykips2030@gmail.com>
2023-11-15 14:12:13 -0800

    Adding scripts.

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const no = Math.floor(Number(process.argv[2]));
+console.log(isNaN(no) ? 'Not a number' : `My number: ${numb}`);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+for (const phrase of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(phrase);
+}
