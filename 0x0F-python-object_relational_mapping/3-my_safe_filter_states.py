5fd65755d0686ea0ce843de6f40405f2c37b0214
GideonMissoy <giddykips2030@gmail.com>
2023-11-17 23:49:45 -0800

    Script takes in args and displays all vals in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script takes in args and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+            passwd=argv[2], db=argv[3], port=3306)
+    curr = db.cursor()
+    match = argv[4]
+    curr.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
