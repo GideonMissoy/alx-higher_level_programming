ac9b5e18aa9ab88e4c8ee159e4a47048f1e47bba
GideonMissoy <giddykips2030@gmail.com>
2023-11-17 23:41:46 -0800

    Script takes in an arg and displays all values in the states table where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script takes in an arg and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+            passwd=argv[2], db=argv[3], port=3306)
+
+    curr = db.cursor()
+    curr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+            .format(argv[4]))
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
